4d6601ded9d323b6cb9efc2c929f7403c0a9ab5b
Ben Foreman <benf.drig@gmail.com>
2025-01-08 10:34:46 -0500

    Create a types.ts file
    This file contains types that are used throughout the app. Types and
    interfaces that are entirely local are still defined within their
    respective files.

---
+++ src/ts/types.ts
@@ -0,0 +1,38 @@
+export type Color = string; // It is assumed to be any CSS-valid color name.
+export type Side = 'start' | 'end';
+
+export type TimeInMS = number; // Time, in milliseconds.
+export type TimeInSeconds = number; // Time, in seconds.
+export type TimeInMinutes = number; // Time, in minutes.
+
+export type IntervalID = number | null; // This is a number that represents a specific Interval object.
+export type TimeoutID = number | null; // This is a number that represents a specific Timeout object.
+
+export interface PersonAppearance {
+  color: Color; // The color of the person. Only used if people are dots.
+  skinColor: Color; // The color of the person's skin.
+  hairColor: Color; // The color of the person's hair.
+  shirtColor: Color; // The color of the person's shirt.
+  pantsColor: Color; // The color of the person's pants.
+}
+
+export interface Person {
+  id: number; // The ID of the person
+  name: string; // The person's name.
+  crossTime: TimeInMinutes; // The time it takes the person to cross the bridge (in minutes).
+  side?: 'start' | 'end'; // The side the person is on.
+}
+
+export interface View {
+  update: (ModelState) => void
+}
+
+declare global {
+  interface Window {
+    appModel: () => void;
+    appController: () => void;
+    appView: () => void;
+  }
+}
+
+export {}
