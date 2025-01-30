1dc03e29f668476f6c18699befffadd35833bc94
Benjamin Foreman <kirbot@gmail.com>
2025-01-29 23:25:30 -0500

    Add tests to make sure controller broadcast works

---
+++ src/ts/__tests__/controller.test.ts
@@ -0,0 +1,39 @@
+import AppController from '../controller';
+import AppView from '../view';
+
+console.log = jest.fn();
+
+describe("Controller", () => {
+
+  let controller: AppController;
+  let views: AppView[];
+
+  describe("Setup and configuration", () => {
+
+    beforeEach(() => {
+      views = [
+        {update: jest.fn()},
+        {update: jest.fn()},
+        {update: jest.fn()},
+      ];
+    });
+
+    it("should automatically subscribe views passed to its constructor", () => {
+      controller = new AppController([views[0]]);
+      const model = {
+        finalState: true,
+        successful: true,
+        peopleAtStart: [],
+        peopleAtEnd: [],
+        timePassed: 0,
+        turnsElapsed: 0,
+        torchSide: 'start',
+      };
+      expect(views[0].update).toHaveBeenCalledWith(model);
+      expect(views[1].update).not.toHaveBeenCalled();
+      expect(views[2].update).not.toHaveBeenCalled();
+    });
+
+  });
+
+});
